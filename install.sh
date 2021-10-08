87411a9ca028a8de3f6236ce07b686cf3eb39543
Ibrahim Habib <engibrahimhabib@gmail.com>
2021-10-08 10:48:23 +0200

    preparing the script

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt get update
+sudo apt-get install python3 python3-venv python3-pip
+sudo apt install git
+git config --global user.name "Ibrahim Habib"
+git config --global user.email "engibrahimhabib@gmail.com"
+ssh-keygen -t ed25519 -C "engibrahimhabib@gmail.com"
+#sudo apt install ./google-chrome-stable_current_amd64.deb
+
